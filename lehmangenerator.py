c7064b275b5a51a9dceeda7f1f4583466e89aa61
pythonindenthater4444 <150388422+pythonindenthater4444@users.noreply.github.com>
2024-01-16 10:31:26 -0500

    Update lehmangenerator.py

--- lehmangenerator.py
+++ lehmangenerator.py
@@ -6,17 +6,17 @@ from io import BytesIO
 
 #########################################
 
-showimage = "true"
-infloop = "true"
+showimage = True
+infloop = True
 loopinterval = 3
 
-while infloop == "true":
+while infloop == True:
   response = requests.get("https://api.github.com/repos/pythonindenthater4444/lehmanapi/contents")
   if response.status_code == 200:
     data = response.json()
     random_image_url = random.choice([i["download_url"] for i in data])
     print(str(random_image_url))
-    if showimage == "true":
+    if showimage == True:
       reponse2 = requests.get(random_image_url)
       if reponse2.status_code == 200:
         img = Image.open(BytesIO(reponse2.content))
@@ -26,5 +26,5 @@ while infloop == "true":
     else:
       print("Error")
   else:
-    print("Error")
+    print("Error: " + str(response.status_code)
   time.sleep(loopinterval)
